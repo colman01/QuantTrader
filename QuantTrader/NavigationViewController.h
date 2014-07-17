6fe545e9bbf2c139a45b67bc038da3dc79554078
colman marcus-quinn <colman01@gmail.com>
2014-07-17 18:18:33 +0200

    Rotation changed

---
+++ QuantTrader/NavigationViewController.h
@@ -0,0 +1,13 @@
+//
+//  NavigationViewController.h
+//  QuantTrader
+//
+//  Created by colman on 17/07/14.
+//  Copyright (c) 2014 Colman Marcus-Quinn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NavigationViewController : UINavigationController
+
+@end
---
+++ QuantTrader/NavigationViewController.m
@@ -0,0 +1,69 @@
+//
+//  NavigationViewController.m
+//  QuantTrader
+//
+//  Created by colman on 17/07/14.
+//  Copyright (c) 2014 Colman Marcus-Quinn. All rights reserved.
+//
+
+#import "NavigationViewController.h"
+#import "BondSetValueViewController.h"
+
+@interface NavigationViewController ()
+
+@end
+
+@implementation NavigationViewController
+
+- (instancetype)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+}
+
+
+
+- (BOOL) shouldAutorotate {
+    UIViewController *vc = self.topViewController;
+
+    if ( ![vc isKindOfClass:[BondSetValueViewController class]]) {
+        
+        return YES;
+    }
+    return NO;
+}
+
+- (BOOL) shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)toInterfaceOrientation {
+    UIViewController *vc = self.topViewController;
+    if ([vc isKindOfClass:[BondSetValueViewController class]])
+        if (toInterfaceOrientation != UIInterfaceOrientationPortrait) {
+            return NO;
+        }
+    return YES;
+    
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
